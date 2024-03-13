2274b27563c98bd7b274b77d55ecb2834b9eb49f
IhsanC22 <145574872+IhsanC22@users.noreply.github.com>
2024-03-13 21:58:43 +0800

    POSTTEST 2

---
+++ postes2 kali ini/postes2 kali ini/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        listperpus = New ListView()
+        btnadd = New Button()
+        btnupdate = New Button()
+        btndelete = New Button()
+        txt_nama = New TextBox()
+        txt_genre = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' listperpus
+        ' 
+        listperpus.FullRowSelect = True
+        listperpus.GridLines = True
+        listperpus.Location = New Point(340, 55)
+        listperpus.Name = "listperpus"
+        listperpus.Size = New Size(439, 303)
+        listperpus.TabIndex = 0
+        listperpus.UseCompatibleStateImageBehavior = False
+        ' 
+        ' btnadd
+        ' 
+        btnadd.BackColor = Color.Orange
+        btnadd.Font = New Font("High Tower Text", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnadd.Location = New Point(29, 282)
+        btnadd.Name = "btnadd"
+        btnadd.Size = New Size(153, 76)
+        btnadd.TabIndex = 1
+        btnadd.Text = "TAMBAH"
+        btnadd.UseVisualStyleBackColor = False
+        ' 
+        ' btnupdate
+        ' 
+        btnupdate.BackColor = Color.Orange
+        btnupdate.Font = New Font("High Tower Text", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnupdate.Location = New Point(457, 382)
+        btnupdate.Name = "btnupdate"
+        btnupdate.Size = New Size(91, 23)
+        btnupdate.TabIndex = 2
+        btnupdate.Text = "PERBARUI"
+        btnupdate.UseVisualStyleBackColor = False
+        ' 
+        ' btndelete
+        ' 
+        btndelete.BackColor = Color.Orange
+        btndelete.Font = New Font("High Tower Text", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btndelete.Location = New Point(607, 382)
+        btndelete.Name = "btndelete"
+        btndelete.Size = New Size(75, 23)
+        btndelete.TabIndex = 3
+        btndelete.Text = "HAPUS"
+        btndelete.UseVisualStyleBackColor = False
+        ' 
+        ' txt_nama
+        ' 
+        txt_nama.BackColor = Color.White
+        txt_nama.Location = New Point(29, 97)
+        txt_nama.Name = "txt_nama"
+        txt_nama.Size = New Size(216, 23)
+        txt_nama.TabIndex = 4
+        ' 
+        ' txt_genre
+        ' 
+        txt_genre.Location = New Point(29, 210)
+        txt_genre.Name = "txt_genre"
+        txt_genre.Size = New Size(216, 23)
+        txt_genre.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("High Tower Text", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Gold
+        Label1.Location = New Point(29, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(139, 22)
+        Label1.TabIndex = 6
+        Label1.Text = "NAMA BUKU"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("High Tower Text", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Gold
+        Label2.Location = New Point(29, 173)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 22)
+        Label2.TabIndex = 7
+        Label2.Text = "GENRE"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.IndianRed
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txt_genre)
+        Controls.Add(txt_nama)
+        Controls.Add(btndelete)
+        Controls.Add(btnupdate)
+        Controls.Add(btnadd)
+        Controls.Add(listperpus)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents listperpus As ListView
+    Friend WithEvents btnadd As Button
+    Friend WithEvents btnupdate As Button
+    Friend WithEvents btndelete As Button
+    Friend WithEvents txt_nama As TextBox
+    Friend WithEvents txt_genre As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ postes2 kali ini/postes2 kali ini/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim index As Integer
+    Dim bebas(2, 100) As String
+    Private Sub cls()
+        txt_nama.Clear()
+        txt_genre.Clear()
+    End Sub
+
+    Private Function cekdatakosong()
+        If txt_nama.Text = "" Then
+            MessageBox.Show("kotak kosong, wajib diisi", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txt_nama.Focus()
+        ElseIf txt_genre.Text = "" Then
+            MessageBox.Show("kotak kosong, wajib diisi", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txt_genre.Focus()
+        Else
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Private Sub nambah_data()
+        listperpus.Items.Clear()
+        For i As Integer = 0 To index - 1
+            Dim item As New ListViewItem((i + 1).ToString())
+            item.SubItems.Add(bebas(1, i))
+            item.SubItems.Add(bebas(2, i))
+            listperpus.Items.Add(item)
+        Next
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "SISTEM MANAGEMEN PERPUSTAKAAN"
+        listperpus.View = View.Details
+        listperpus.Columns.Add("no", 50, HorizontalAlignment.Center)
+        listperpus.Columns.Add("nama", 200, HorizontalAlignment.Center)
+        listperpus.Columns.Add("genre", 200, HorizontalAlignment.Center)
+        txt_nama.Focus()
+    End Sub
+
+    Private Sub mylistview_SelectedIndexChanged(sender As Object, e As EventArgs) Handles listperpus.SelectedIndexChanged
+        If listperpus.SelectedItems.Count > 0 Then
+            txt_nama.Text = listperpus.SelectedItems(0).SubItems(1).Text
+            txt_genre.Text = listperpus.SelectedItems(0).SubItems(2).Text
+        End If
+    End Sub
+
+    Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
+        If cekdatakosong() = True Then
+            If index < 100 Then
+                Dim nama As String = txt_nama.Text
+                Dim genre As String = txt_genre.Text
+
+                bebas(1, index) = nama
+                bebas(2, index) = genre
+
+                index += 1
+
+                nambah_data()
+
+                cls()
+            Else
+                MessageBox.Show("perpus kelebihan", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        End If
+    End Sub
+
+    Private Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        If listperpus.SelectedItems.Count > 0 Then
+            Dim ok As String
+            ok = MessageBox.Show("yakin mau dihapus ?", "info", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If ok = MsgBoxResult.Yes Then
+                Dim selectedindex As Integer = listperpus.SelectedIndices(0)
+                For i As Integer = selectedindex To index - 2
+                    bebas(1, i) = bebas(1, i + 1)
+                    bebas(2, i) = bebas(2, i + 1)
+                Next
+                index -= 1
+                nambah_data()
+                MessageBox.Show("buku berhasil dihapus", "info", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+            End If
+        Else
+            MessageBox.Show("pilih buku yg mau dihapus", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnupdate_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
+        If listperpus.SelectedItems.Count > 0 Then
+            If cekdatakosong() = True Then
+                Dim selectedindex As Integer = listperpus.SelectedIndices(0)
+                Dim namabaru As String = txt_nama.Text
+                Dim genrebaru As String = txt_genre.Text
+
+                bebas(1, selectedindex) = namabaru
+                bebas(2, selectedindex) = genrebaru
+
+                nambah_data()
+
+                cls()
+                MessageBox.Show("data berhasil diganti", "info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Else
+            MessageBox.Show("pilih buku yang mau di ganti", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
