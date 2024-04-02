abb715115c9d8832f872470f8593764c3e710779
IhsanC22 <145574872+IhsanC22@users.noreply.github.com>
2024-04-02 22:32:51 +0800

    Add files via upload

---
+++ POSTTEST5/posttest5/review empat/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtkodejenis = New System.Windows.Forms.TextBox()
+        Me.txtjenis = New System.Windows.Forms.TextBox()
+        Me.btnsimpan = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btnubah = New System.Windows.Forms.Button()
+        Me.btnbatal = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jenis"
+        '
+        'txtkodejenis
+        '
+        Me.txtkodejenis.Location = New System.Drawing.Point(144, 110)
+        Me.txtkodejenis.Name = "txtkodejenis"
+        Me.txtkodejenis.Size = New System.Drawing.Size(313, 20)
+        Me.txtkodejenis.TabIndex = 2
+        '
+        'txtjenis
+        '
+        Me.txtjenis.Location = New System.Drawing.Point(144, 175)
+        Me.txtjenis.Name = "txtjenis"
+        Me.txtjenis.Size = New System.Drawing.Size(313, 20)
+        Me.txtjenis.TabIndex = 3
+        '
+        'btnsimpan
+        '
+        Me.btnsimpan.BackColor = System.Drawing.SystemColors.Desktop
+        Me.btnsimpan.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnsimpan.Location = New System.Drawing.Point(144, 222)
+        Me.btnsimpan.Name = "btnsimpan"
+        Me.btnsimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnsimpan.TabIndex = 5
+        Me.btnsimpan.Text = "simpan"
+        Me.btnsimpan.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(520, 141)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(387, 287)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(129, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(735, 33)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "APLIKASI PENDATAAN TOKO DAGING CV.MULYA"
+        '
+        'btnhapus
+        '
+        Me.btnhapus.BackColor = System.Drawing.SystemColors.Desktop
+        Me.btnhapus.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnhapus.Location = New System.Drawing.Point(420, 355)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnhapus.TabIndex = 11
+        Me.btnhapus.Text = "Hapus"
+        Me.btnhapus.UseVisualStyleBackColor = False
+        '
+        'btnubah
+        '
+        Me.btnubah.BackColor = System.Drawing.SystemColors.Desktop
+        Me.btnubah.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnubah.Location = New System.Drawing.Point(420, 326)
+        Me.btnubah.Name = "btnubah"
+        Me.btnubah.Size = New System.Drawing.Size(75, 23)
+        Me.btnubah.TabIndex = 12
+        Me.btnubah.Text = "Edit"
+        Me.btnubah.UseVisualStyleBackColor = False
+        '
+        'btnbatal
+        '
+        Me.btnbatal.BackColor = System.Drawing.SystemColors.Desktop
+        Me.btnbatal.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnbatal.Location = New System.Drawing.Point(420, 384)
+        Me.btnbatal.Name = "btnbatal"
+        Me.btnbatal.Size = New System.Drawing.Size(75, 23)
+        Me.btnbatal.TabIndex = 13
+        Me.btnbatal.Text = "Batal"
+        Me.btnbatal.UseVisualStyleBackColor = False
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(520, 110)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(387, 20)
+        Me.txtSearch.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(934, 430)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnbatal)
+        Me.Controls.Add(Me.btnubah)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnsimpan)
+        Me.Controls.Add(Me.txtjenis)
+        Me.Controls.Add(Me.txtkodejenis)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtkodejenis As TextBox
+    Friend WithEvents txtjenis As TextBox
+    Friend WithEvents btnsimpan As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnhapus As Button
+    Friend WithEvents btnubah As Button
+    Friend WithEvents btnbatal As Button
+    Friend WithEvents txtSearch As TextBox
+End Class
---
+++ POSTTEST5/posttest5/review empat/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Sub Kosong()
+        txtjenis.Clear()
+        txtkodejenis.Clear()
+        txtjenis.Focus()
+    End Sub
+    Sub isi()
+        txtjenis.Clear()
+        txtjenis.Focus()
+    End Sub
+    Sub tampilJenis()
+        DA = New MySqlDataAdapter("select * from daging", CONN)
+        DS = New DataSet
+        DS.Clear()
+        DA.Fill(DS, "jenis")
+        DataGridView1.DataSource = DS.Tables("jenis")
+        DataGridView1.Refresh()
+    End Sub
+    Sub aturGrid()
+        DataGridView1.Columns(0).Width = 60
+        DataGridView1.Columns(1).Width = 200
+        DataGridView1.Columns(0).HeaderText = "ID"
+        DataGridView1.Columns(1).HeaderText = "Jenis"
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        koneksi()
+        tampilJenis()
+        Kosong()
+        aturGrid()
+    End Sub
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnsimpan.Click
+        If txtjenis.Text = Nothing And txtkodejenis.Text = Nothing Then
+            MsgBox("Data Belum Lengkap")
+            txtkodejenis.Focus()
+        Else
+            CMD = New MySqlCommand("Select * from daging where id =
+'" & txtkodejenis.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            If Not RD.HasRows Then
+                RD.Close()
+                CMD = New MySqlCommand("insert into daging values
+('" & txtkodejenis.Text & "', '" & txtjenis.Text &
+                "')", CONN)
+                CMD.ExecuteNonQuery()
+                tampilJenis()
+                Kosong()
+                MsgBox("Simpan Data Sukses!")
+                txtkodejenis.Focus()
+            Else
+                RD.Close()
+                MsgBox("Data Tersebut Sudah Ada")
+            End If
+        End If
+    End Sub
+    Private Sub txtjenis_KeyPress(sender As Object, e As KeyPressEventArgs)
+        txtjenis.MaxLength = 50
+        If e.KeyChar = Chr(13) Then
+            txtjenis.Text = UCase(txtjenis.Text)
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim row As DataGridViewRow = DataGridView1.SelectedRows(0)
+            If row.Index < DataGridView1.RowCount - 1 And row.Index >= 0 Then
+                txtkodejenis.Text = row.Cells(0).Value
+                txtjenis.Text = row.Cells(1).Value
+            End If
+        End If
+    End Sub
+
+    Private Sub btnubah_Click(sender As Object, e As EventArgs) Handles btnubah.Click
+        If txtjenis.Text = Nothing Then
+            MsgBox("Jenis belum diisi")
+            txtkodejenis.Focus()
+        Else
+            Dim ubah As String = "update daging set jenis = '" &
+            txtjenis.Text & "' where id = '" & txtkodejenis.Text & "'"
+            CMD = New MySqlCommand(ubah, CONN)
+            CMD.ExecuteNonQuery()
+            MsgBox("Data berhasil diubah")
+            tampilJenis()
+            Kosong()
+        End If
+
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        If txtjenis.Text Is Nothing Then
+            MsgBox("Jenis belum diisi")
+            txtkodejenis.Focus()
+        Else
+            Dim hapus As String = "delete from daging where id = '" & txtkodejenis.Text & "'"
+            CMD = New MySqlCommand(hapus, CONN)
+            CMD.ExecuteNonQuery()
+            MsgBox("Data berhasil dihapus")
+            tampilJenis()
+            Kosong()
+        End If
+    End Sub
+
+    Private Sub btnbatal_Click(sender As Object, e As EventArgs) Handles btnbatal.Click
+        Kosong()
+        tampilJenis()
+    End Sub
+
+    Private Sub txtKodejenis_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtkodejenis.KeyPress
+        txtkodejenis.MaxLength = 2
+        If e.KeyChar = Chr(13) Then
+            CMD = New MySqlCommand("select * from daging", CONN)
+            RD = CMD.ExecuteReader()
+            RD.Read()
+            If RD.HasRows Then
+                txtjenis.Text = RD("jenis")
+                txtjenis.Focus()
+            Else
+                isi()
+                txtjenis.Focus()
+            End If
+            RD.Close()
+        End If
+
+
+    End Sub
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+        If txtSearch.Text <> Nothing Then
+            CMD = New MySqlCommand("select * from daging where id like '%" & txtSearch.Text & "%'", CONN)
+            RD = CMD.ExecuteReader()
+            If RD.HasRows Then
+                DS.Clear()
+                While RD.Read()
+                    Dim dsNewRow As DataRow = DS.Tables("jenis").NewRow()
+                    dsNewRow.Item(0) = RD("id")
+                    dsNewRow.Item(1) = RD("jenis")
+                    DS.Tables("jenis").Rows.Add(dsNewRow)
+                End While
+            Else
+                MsgBox("Data tidak ditemukan")
+            End If
+            RD.Close()
+        Else
+            tampilJenis()
+        End If
+    End Sub
+End Class
